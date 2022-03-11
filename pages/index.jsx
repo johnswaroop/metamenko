24615de0763bda5d3ce8118eac8c027ad0910dd7
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-03-11 12:21:04 +0530

    footer push

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,6 +18,10 @@ import allSocialIcons from './assets/follow-us-icons/Follow-us-graphics.png'
 import mediumIcon from './assets/follow-us-icons/Subtract.png'
 import twitterIcon from './assets/follow-us-icons/Subtract-1.png'
 import discordIcon from './assets/follow-us-icons/Twitter social icons - rounded square - white 1.png'
+import discord from './assets/discord.png'
+import twitter from './assets/twitter.png'
+import medium from './assets/medium.png'
+
 
 function importAll(r) {
   let images = {};
@@ -52,78 +56,79 @@ const questions =
 
 function Home() {
   return (
-    <div className={styles.page}>
+    <>
+      <div className={styles.page}>
 
-      <section className={styles.sec1}>
-        <Nav></Nav>
-        <img src={dragon.src} alt="" className={styles.float_dragon} />
-        <img src={egg.src} alt="" className={styles.float_egg} />
-        <img src={fan.src} alt="" className={styles.float_fan} />
-        <img src={heroLogo.src} alt="" className={styles.float_heroLogo} />
-        <div className={styles.subTitle}>
-          <h3>Tear open a mystery pack</h3>
-          <h3>AND enter the Menkoverse</h3>
-          <p>A reimagined centuries-old game, MetaMenko combines strategy and chance to battle against Shadow Lords and free entrapped Menko spirits. Win battles to unlock secret vaults holding hidden, rare NFTs and other valuable treasures.</p>
-          <button>JOIN THE WHITELIST</button>
-        </div>
-      </section>
+        <section className={styles.sec1}>
+          <Nav></Nav>
+          <img src={dragon.src} alt="" className={styles.float_dragon} />
+          <img src={egg.src} alt="" className={styles.float_egg} />
+          <img src={fan.src} alt="" className={styles.float_fan} />
+          <img src={heroLogo.src} alt="" className={styles.float_heroLogo} />
+          <div className={styles.subTitle}>
+            <h3>Tear open a mystery pack</h3>
+            <h3>AND enter the Menkoverse</h3>
+            <p>A reimagined centuries-old game, MetaMenko combines strategy and chance to battle against Shadow Lords and free entrapped Menko spirits. Win battles to unlock secret vaults holding hidden, rare NFTs and other valuable treasures.</p>
+            <button>JOIN THE WHITELIST</button>
+          </div>
+        </section>
 
-      <section className={styles.sec2}>
-        <div className={styles.statRow}>
-          <div className={styles.stat}>
-            <p>PROJECT SIZE</p>
-            <h1>7,000</h1>
-            <h4>MYSTERY PACKS</h4>
-          </div>
-          <div className={styles.stat}>
-            <p>MINT PRICE</p>
-            <h1>TBD</h1>
-            <h4>ON SOLANA</h4>
-          </div>
-          <div className={styles.stat}>
-            <p>MINT DATE</p>
-            <h1>TBD</h1>
-            <h4>IN Q2 2022</h4>
-          </div>
-        </div>
-
-        <div className={styles.nftSlider}>
-          <div className={styles.nftCoinRow1}>
-            {
-              Object.values(r1_images).map((ele, i) => {
-                return (
-                  <div key={'r' + i} className={styles.coin}>
-                    <img src={ele.default.src} alt="" />
-                  </div>
-                )
-              })
-            }
+        <section className={styles.sec2}>
+          <div className={styles.statRow}>
+            <div className={styles.stat}>
+              <p>PROJECT SIZE</p>
+              <h1>7,000</h1>
+              <h4>MYSTERY PACKS</h4>
+            </div>
+            <div className={styles.stat}>
+              <p>MINT PRICE</p>
+              <h1>TBD</h1>
+              <h4>ON SOLANA</h4>
+            </div>
+            <div className={styles.stat}>
+              <p>MINT DATE</p>
+              <h1>TBD</h1>
+              <h4>IN Q2 2022</h4>
+            </div>
           </div>
 
-          <div className={styles.nftCoinRow2}>
-            {
-              Object.values(r2_images).map((ele, i) => {
-                return (
-                  <div key={'r' + i} className={styles.coin}>
-                    <img src={ele.default.src} alt="" />
-                  </div>
-                )
-              })
-            }
+          <div className={styles.nftSlider}>
+            <div className={styles.nftCoinRow1}>
+              {
+                Object.values(r1_images).map((ele, i) => {
+                  return (
+                    <div key={'r' + i} className={styles.coin}>
+                      <img src={ele.default.src} alt="" />
+                    </div>
+                  )
+                })
+              }
+            </div>
+
+            <div className={styles.nftCoinRow2}>
+              {
+                Object.values(r2_images).map((ele, i) => {
+                  return (
+                    <div key={'r' + i} className={styles.coin}>
+                      <img src={ele.default.src} alt="" />
+                    </div>
+                  )
+                })
+              }
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section className={styles.sec3}>
-        <img src={leftHero.src} alt="" className={styles.leftHero} />
-        <div className={styles.sec3text}>
-          <h1>WHAT IS METAMENKO?</h1>
-          <p>MetaMenko is the first ever gaming metaverse built on the Solana blockchain with 7,000 unique mystery packs that can be burned to reveal Menko NFTs with in-game and real-world utility.</p>
-          <h1 className={styles.heading2} >OUR MISSION STATEMENT</h1>
-          <p>Spotlight worlds imagined by under-represented artists and bring these worlds to life in a competitive strategy-based game built on the Solana blockchain. We reward long term NFT holders via passive income, DAO community membership & voice, and additional utility.</p>
-        </div>
-      </section>
-
+        <section className={styles.sec3}>
+          <img src={leftHero.src} alt="" className={styles.leftHero} />
+          <div className={styles.sec3text}>
+            <h1>WHAT IS METAMENKO?</h1>
+            <p>MetaMenko is the first ever gaming metaverse built on the Solana blockchain with 7,000 unique mystery packs that can be burned to reveal Menko NFTs with in-game and real-world utility.</p>
+            <h1 className={styles.heading2} >OUR MISSION STATEMENT</h1>
+            <p>Spotlight worlds imagined by under-represented artists and bring these worlds to life in a competitive strategy-based game built on the Solana blockchain. We reward long term NFT holders via passive income, DAO community membership & voice, and additional utility.</p>
+          </div>
+        </section>
+      </div>
       <section className={styles.sec4}>
         <div className={styles.titleCon}>
           <p className={styles.subTitle}>NFT</p>
@@ -292,7 +297,25 @@ function Home() {
         </div>
       </section>
 
-    </div>
+      <section className={styles.footer}>
+        <img src={heroLogo.src} alt="" />
+        <p>ENTER THE MENKOVERSE</p>
+        <ul className={styles.iconsFooter}>
+          <li>
+            <img src={medium.src} alt="" />
+          </li>
+
+          <li>
+            <img src={discord.src} alt="" />
+          </li>
+
+          <li>
+            <img src={twitter.src} alt="" />
+          </li>
+
+        </ul>
+      </section>
+    </>
   )
 }
 
